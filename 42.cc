3fb3d55102319c3c17cc18ef8350f33fe39ac0c7
Aishwarya Singh <aishwarya4444@gmail.com>
2017-08-21 02:12:20 +0530

    http://www.spoj.com/problems/ADDREV/

---
+++ 42.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(void )
+{
+   int N, a, b, A, B, x, sum;
+   cin>>N;
+   while(N--)
+   {
+      cin>>a>>b;
+      x=0,sum=0,A=1,B=1;
+      while(a/A>9)A*=10;
+      while(b/B>9)B*=10;
+      while(a || b)
+      {
+         if(a)sum+=a/A,a%=A,A/=10;
+         if(b)sum+=b/B,b%=B,B/=10;
+         x=10*x+(sum%10);
+         sum/=10;
+      }
+      if(sum)x=10*x+sum;
+      cout<<x<<endl;
+   }
+}
