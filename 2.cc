cf4b3bb83fb3d99d3eb3543949ba97eb19326fe2
Aishwarya Singh <aishwarya4444@gmail.com>
2017-08-21 02:35:30 +0530

    http://www.spoj.com/problems/FCTRL/

--- 2.cc
+++ 2.cc
@@ -7,27 +7,27 @@ typedef unsigned long long int ULLI;
 bool primalityTest(ULLI i)
 {
     if(i==1)return false;
-	if(i==2 || i==3)return true;
-	if(i%2==0 || i%3==0)return false;
-	for(ULLI j=5;j*j<=i;j+=6)
-	{
-	    if(i%(j)==0 || i%(j+2)==0)return false;
-	}
-	return true;
+    if(i==2 || i==3)return true;
+    if(i%2==0 || i%3==0)return false;
+    for(ULLI j=5;j*j<=i;j+=6)
+    {
+        if(i%(j)==0 || i%(j+2)==0)return false;
+    }
+    return true;
 }
 
 int main( void)
 {
     ULLI t, i, l, r;
-	cin>>t;
-	while(t--)
+    cin>>t;
+    while(t--)
     {
         cin>>l>>r;;
         for(i=l;i<=r;i++)
-		{
-		    if(primalityTest(i))cout<<i<<'\n';
-	    }
-		cout<<endl;
+        {
+            if(primalityTest(i))cout<<i<<'\n';
+        }
+        cout<<endl;
     }
     return 0;
 }
