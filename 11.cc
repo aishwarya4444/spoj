8ee9422adddfb194a243538ab12e2ce8666a61e0
Aishwarya Singh <aishwarya4444@gmail.com>
2017-08-21 02:09:35 +0530

    http://www.spoj.com/problems/FCTRL/

---
+++ 11.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(void )
+{
+   long long t, n;
+   size_t count;
+   cin>>t;
+   while(t--)
+   {
+      cin>>n;
+      count=0;
+      while(n)
+      {
+         count+=n/5;
+         n/=5;
+      }
+      cout<<count<<endl;
+   }
+   return 0;
+}
